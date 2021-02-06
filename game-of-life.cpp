fae436a34a92e427436918f9a5d1ab6fb9dff4e5
TanishqSingla <tanishqsingla00@gmail.com>
2021-02-07 01:12:09 +0530

    replacing iostream with ncurses

--- game-of-life.cpp
+++ game-of-life.cpp
@@ -1,84 +1,98 @@
-#include <iostream>
+#include <ncurses.h>
 
-bool is_ok(int n) {
+using namespace std;
+
+bool is_ok(int n)
+{
 	return n % 2 == 0 || n % 7 == 0;
 }
 
-class Universe {
+class Universe
+{
 private:
 	int row;
 	int col;
 	int cells[1000][1000];
 
 public:
-	
-
-	Universe() {
+	Universe()
+	{
 		row = 32;
 		col = 32;
-		
-		for (int i = 0; i < row; i++) {
-			for (int j = 0; j < col; j++) {
-				if(is_ok(i + j) || is_ok(i + j))
+
+		for (int i = 0; i < row; i++)
+		{
+			for (int j = 0; j < col; j++)
+			{
+				if (is_ok(i + j) || is_ok(i + j))
 					cells[i][j] = 1;
 				else
 					cells[i][j] = 0;
 			}
 		}
 	}
-	
-	void tick() {
+
+	void tick()
+	{
 		int count = 0;
-		for (int i = 0; i < row; i++) {
-			for (int j = 0; j < col; j++) {
+		for (int i = 0; i < row; i++)
+		{
+			for (int j = 0; j < col; j++)
+			{
 				count = 0;
-				count += cells[i-1][j-1];
-				count += cells[i-1][j];
-				count += cells[i-1][j+1];
-				count += cells[i][j-1];
-				count += cells[i][j+1];
-				count += cells[i+1][j-1];
-				count += cells[i+1][j];
-				count += cells[i+1][j+1];
-				
-				if(count < 0 || count > 8)
+				count += cells[i - 1][j - 1];
+				count += cells[i - 1][j];
+				count += cells[i - 1][j + 1];
+				count += cells[i][j - 1];
+				count += cells[i][j + 1];
+				count += cells[i + 1][j - 1];
+				count += cells[i + 1][j];
+				count += cells[i + 1][j + 1];
+
+				if (count < 0 || count > 8)
 					count = 0;
-				
-				
-				if(cells[i][j] == 1 && count < 2)
+
+				if (cells[i][j] == 1 && count < 2)
 					cells[i][j] = 0;
-				else if(cells[i][j] == 1 && (count == 2 || count == 3))
+				else if (cells[i][j] == 1 && (count == 2 || count == 3))
 					cells[i][j] = 1;
-				else if(cells[i][j] == 1 && count > 3)
+				else if (cells[i][j] == 1 && count > 3)
 					cells[i][j] = 0;
-				else if(cells[i][j] == 0 && count == 3)
+				else if (cells[i][j] == 0 && count == 3)
 					cells[i][j] = 1;
 			}
 		}
 	}
-	 
-	void render() {
-		for (int i = 0; i < row; i++) {
-			std::cout << "\t";
-			for (int j = 0; j < col; j++) {
-				cells[i][j] ? std::cout << "■ " : std::cout << "□ ";
-			}
-			std::cout << std::endl;
-		}
-	}
-	
+
+	// void render()
+	// {
+	// 	for (int i = 0; i < row; i++)
+	// 	{
+	// 		std::cout << "\t";
+	// 		for (int j = 0; j < col; j++)
+	// 		{
+	// 			cells[i][j] ? std::cout << "■ " : std::cout << "□ ";
+	// 		}
+	// 		std::cout << std::endl;
+	// 	}
+	// }
 };
 
-int main() {
+int main(int argc, char **argv)
+{
+	initscr();
 	Universe universe = Universe();
-	
-	universe.render();
-	
-	while(true) {
-		universe.tick();
-		universe.render();
-		system("clear");
-	}
-	
+
+	// universe.render();
+
+	// while (true)
+	// {
+	// 	universe.tick();
+	// 	universe.render();
+	// 	system("clear");
+	// }
+	getch();
+	endwin();
+
 	return 0;
 }
